f827df320407de01b3ea299809cf8330b1a17f8b
Amy Ousterhout <aousterh@princeton.edu>
2012-04-04 21:44:12 -0400

    small changes to plotting stuff

--- plotting/plot.py
+++ plotting/plot.py
@@ -9,7 +9,7 @@ sub1 = fig1.add_subplot(211)
 r = mlab.csv2rec("../momose/output/scramRecorderOutput.csv", skiprows=3)
 x = r.latency
 
-n, bins, patches = sub1.hist(x, 5, normed=1, cumulative=False, histtype='bar', alpha=0.75)
+n, bins, patches = sub1.hist(x, 10, normed=1, cumulative=False, histtype='bar', alpha=0.75)
 sub1.set_xlim(min(x), max(x))
 sub1.set_ylim(0, max(n) * 1.1)
 
@@ -21,6 +21,6 @@ y_data = np.cumsum(y)
 
 sub2.plot(x_data, y_data, 'o-')
 sub2.set_xlim(0, max(x_data))
-
+sub2.set_ylim(0, 1.2)
 
 plt.show()
